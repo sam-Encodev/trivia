a6670055d04f0a96e457c88d7210b02765e0e70d
Sam <samuel@encodevlabs.com>
2024-10-31 20:54:24 +0000

    view questions cleanup

--- lib/components/view_question.dart
+++ lib/components/view_question.dart
@@ -7,15 +7,15 @@ import 'package:trivia/providers/response_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class ViewQuestion extends ConsumerStatefulWidget {
+  final int index;
+  final dynamic data;
+  final int length;
+
   const ViewQuestion(
       {super.key,
       required this.index,
       required this.data,
       required this.length});
-
-  final int index;
-  final Map data;
-  final int length;
 
   @override
   ConsumerState<ViewQuestion> createState() => _QuestionState();
@@ -28,23 +28,23 @@ class _QuestionState extends ConsumerState<ViewQuestion> {
   Widget build(BuildContext context) {
     var index = widget.index.toString();
     var totalQuestions = widget.length.toString();
-    var correctAnswer = widget.data["correctAnswer"];
-    var incorrectAnswers = widget.data["incorrectAnswers"];
+    var correctAnswer = widget.data.correctAnswer;
+    var incorrectAnswers = widget.data.incorrectAnswers;
 
     final List<String> options = [correctAnswer, ...incorrectAnswers];
     // options.shuffle();
 
     var info = Question(
-      id: widget.data["id"],
-      category: widget.data["category"],
-      correctAnswer: widget.data["correctAnswer"],
-      incorrectAnswers: widget.data["incorrectAnswers"],
-      question: widget.data["question"],
-      type: widget.data["type"],
-      tags: widget.data["tags"],
-      difficulty: widget.data["difficulty"],
-      regions: widget.data["regions"],
-      isNiche: widget.data["isNiche"],
+      id: widget.data.id,
+      category: widget.data.category,
+      correctAnswer: widget.data.correctAnswer,
+      incorrectAnswers: widget.data.incorrectAnswers,
+      question: widget.data.question,
+      type: widget.data.type,
+      tags: widget.data.tags,
+      difficulty: widget.data.difficulty,
+      regions: widget.data.regions,
+      isNiche: widget.data.isNiche,
       options: options,
     );
 
@@ -79,45 +79,50 @@ class _QuestionState extends ConsumerState<ViewQuestion> {
             height: 30.0,
           ),
           Wrap(
+            crossAxisAlignment: WrapCrossAlignment.center,
             direction: Axis.vertical,
             spacing: standardSpacing,
             children: info.options!.asMap().entries.map((entry) {
-              return SizedBox(
-                width: 250.0,
-                child: InputChip(
-                  label: Text(
+              return InputChip(
+                label: SizedBox(
+                  width: 300.0,
+                  child: Text(
+                    textAlign: TextAlign.center,
                     entry.value,
+                    maxLines: 3,
+                    overflow: TextOverflow.clip,
+                    softWrap: false,
                   ),
-                  showCheckmark: false,
-                  selected: selectedIndex == entry.key,
-                  onSelected: (bool selected) {
-                    setState(() {
-                      if (selectedIndex == entry.key) {
-                        selectedIndex = null;
-                        ref
-                            .read(responseNotifierProvider.notifier)
-                            .removeResponse(SelectedResponse(
+                ),
+                showCheckmark: false,
+                selected: selectedIndex == entry.key,
+                onSelected: (bool selected) {
+                  setState(() {
+                    if (selectedIndex == entry.key) {
+                      selectedIndex = null;
+                      ref
+                          .read(responseNotifierProvider.notifier)
+                          .removeResponse(SelectedResponse(
+                            userID: 1,
+                            questionID: info.id,
+                            id: int.tryParse(index),
+                          ));
+                    } else {
+                      selectedIndex = entry.key;
+                      ref.read(responseNotifierProvider.notifier).addResponse(
+                          SelectedResponse(
                               userID: 1,
                               questionID: info.id,
+                              options: info.options,
                               id: int.tryParse(index),
-                            ));
-                      } else {
-                        selectedIndex = entry.key;
-                        ref.read(responseNotifierProvider.notifier).addResponse(
-                            SelectedResponse(
-                                userID: 1,
-                                questionID: info.id,
-                                options: info.options,
-                                id: int.tryParse(index),
-                                question: info.question,
-                                selectedOption: entry.value,
-                                createdAt: DateTime.timestamp().toString(),
-                                updatedAt: DateTime.timestamp().toString(),
-                                correctAnswer: info.correctAnswer));
-                      }
-                    });
-                  },
-                ),
+                              question: info.question,
+                              selectedOption: entry.value,
+                              createdAt: DateTime.timestamp().toString(),
+                              updatedAt: DateTime.timestamp().toString(),
+                              correctAnswer: info.correctAnswer));
+                    }
+                  });
+                },
               );
             }).toList(),
           ),
