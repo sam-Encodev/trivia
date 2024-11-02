f95ada42620a14d5a82e7f9ba209a612be8ade32
Sam <samuel@encodevlabs.com>
2024-11-02 12:49:18 +0000

    updated text

--- lib/constants/text.dart
+++ lib/constants/text.dart
@@ -1,6 +1,8 @@
 const slash = ' / ';
 const reset = "reset";
 const review = "Review";
+const result = "View Results";
 const questions = "Questions";
+const congratsMessage = "Congrats, Champ!";
 const completionEntry = "Trivia complete";
 const errorEntry = "Select an option to continue.";
