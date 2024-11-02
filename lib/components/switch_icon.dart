115bf21c5aa2bb4f8b7ffa0647d6d8952cd7196a
Sam <samuel@encodevlabs.com>
2024-11-02 12:48:53 +0000

    switch icon update

--- lib/components/switch_icon.dart
+++ lib/components/switch_icon.dart
@@ -5,11 +5,13 @@ import 'package:trivia/constants/spacing.dart';
 switchIcon(context, message) {
   switch (message) {
     case completionEntry:
+    case true:
       return const Icon(Icons.check_circle_outline_outlined,
           size: standardIcon, color: Colors.green);
 
     case errorEntry:
-      return Icon(Icons.error_outline_outlined,
-          size: standardIcon, color: Theme.of(context).colorScheme.error);
+    case false:
+      return const Icon(Icons.error_outline_outlined,
+          size: standardIcon, color: Colors.pink);
   }
 }
