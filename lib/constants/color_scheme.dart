81cb03e383e6cc70d173e515e522abae477976b3
Sam <samuel@encodevlabs.com>
2024-11-16 10:49:28 +0000

    flex_color_scheme & google_fonts

---
+++ lib/constants/color_scheme.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flex_color_scheme/flex_color_scheme.dart';
+
+bool useMaterial3 = true;
+const appBarBackground = Colors.transparent;
+const scaffoldBackground = Colors.transparent;
+const FlexScheme _scheme = FlexScheme.blackWhite;
+final String? _fontFamily = GoogleFonts.notoSans().fontFamily;
+
+const TextTheme _textTheme = TextTheme(
+  displayLarge: TextStyle(fontSize: 57),
+  displayMedium: TextStyle(fontSize: 45),
+  displaySmall: TextStyle(fontSize: 36),
+  headlineLarge: TextStyle(fontSize: 32),
+  headlineMedium: TextStyle(fontSize: 28),
+  headlineSmall: TextStyle(fontSize: 24),
+  titleLarge: TextStyle(fontSize: 22),
+  titleMedium: TextStyle(fontSize: 16),
+  titleSmall: TextStyle(fontSize: 14),
+  bodyLarge: TextStyle(fontSize: 16),
+  bodyMedium: TextStyle(fontSize: 14),
+  bodySmall: TextStyle(fontSize: 12),
+  labelLarge: TextStyle(fontSize: 14, letterSpacing: 0.5),
+  labelMedium: TextStyle(fontSize: 12),
+  labelSmall: TextStyle(fontSize: 11),
+);
+
+sealed class Themer {
+  // The defined light theme.
+  static ThemeData light = FlexThemeData.light(
+    scheme: _scheme,
+    useMaterial3: useMaterial3,
+    appBarBackground: appBarBackground,
+    scaffoldBackground: scaffoldBackground,
+    fontFamily: _fontFamily,
+    textTheme: _textTheme,
+    primaryTextTheme: _textTheme,
+    subThemesData: const FlexSubThemesData(
+      interactionEffects: true,
+      tintedDisabledControls: true,
+      useM2StyleDividerInM3: true,
+      inputDecoratorIsFilled: true,
+      inputDecoratorBorderType: FlexInputBorderType.outline,
+      alignedDropdown: true,
+      navigationRailUseIndicator: true,
+      navigationRailLabelType: NavigationRailLabelType.all,
+    ),
+    visualDensity: FlexColorScheme.comfortablePlatformDensity,
+    cupertinoOverrideTheme: const CupertinoThemeData(applyThemeToAll: true),
+  );
+  // The defined dark theme.
+  static ThemeData dark = FlexThemeData.dark(
+    scheme: _scheme,
+    useMaterial3: useMaterial3,
+    scaffoldBackground: scaffoldBackground,
+    fontFamily: _fontFamily,
+    textTheme: _textTheme,
+    primaryTextTheme: _textTheme,
+    subThemesData: const FlexSubThemesData(
+      interactionEffects: true,
+      tintedDisabledControls: true,
+      blendOnColors: true,
+      useM2StyleDividerInM3: true,
+      inputDecoratorIsFilled: true,
+      inputDecoratorBorderType: FlexInputBorderType.outline,
+      alignedDropdown: true,
+      navigationRailUseIndicator: true,
+      navigationRailLabelType: NavigationRailLabelType.all,
+    ),
+    visualDensity: FlexColorScheme.comfortablePlatformDensity,
+    cupertinoOverrideTheme: const CupertinoThemeData(applyThemeToAll: true),
+  );
+}
--- lib/trivia.dart
+++ lib/trivia.dart
@@ -1,22 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:trivia/routes/router.dart';
+import 'package:trivia/providers/app_theme.dart';
+import 'package:trivia/constants/color_scheme.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class Trivia extends StatelessWidget {
+class Trivia extends ConsumerWidget {
   const Trivia({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final theme = ref.watch(appTheme);
+
     return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
       title: 'Trivia',
+      themeMode: theme,
+      theme: Themer.light,
       routerConfig: router,
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(backgroundColor: Colors.transparent),
-        scaffoldBackgroundColor: Colors.transparent,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      darkTheme: Themer.dark,
+      debugShowCheckedModeBanner: false,
     );
   }
 }
