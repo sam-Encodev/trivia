73b6987ec25f33803cd4eb9bace3534ff9cb2285
Sam <samuel@encodevlabs.com>
2024-11-29 14:37:11 +0000

    home update

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:trivia/components/category_list.dart';
+import 'package:trivia/constants/questions.dart';
 import 'package:trivia/constants/spacing.dart';
 import 'package:trivia/constants/styles.dart';
 import 'package:trivia/providers/daily_trivia.dart';
@@ -28,7 +30,7 @@ class _HomeState extends ConsumerState<Home> {
         actions: [
           IconButton(
             tooltip: "Daily Trivia",
-            icon: const Icon(Icons.wifi_protected_setup_sharp),
+            icon: const Icon(Icons.wifi_protected_setup_outlined),
             onPressed: () =>
                 {ref.read(dailyTriviaQuestions.notifier).shuffle()},
           ),
@@ -39,46 +41,111 @@ class _HomeState extends ConsumerState<Home> {
           ),
         ],
       ),
-      body: Container(
-          height: MediaQuery.of(context).size.height / 1.4,
-          padding: const EdgeInsets.symmetric(vertical: standardSpacing),
-          decoration: BoxDecoration(
-            borderRadius: const BorderRadius.only(
-              topLeft: Radius.circular(minRadius),
-              topRight: Radius.circular(minRadius),
-              bottomLeft: Radius.circular(minRadius),
-              bottomRight: Radius.circular(minRadius),
-            ),
-            color: Theme.of(context).colorScheme.surfaceContainerHigh,
-          ),
-          child: settings
-              ? ListView.separated(
-                  itemCount: questions.length.toInt(),
-                  itemBuilder: (BuildContext context, int index) {
-                    return ListTile(
-                      title: Text(
-                        questions[index].question,
-                        style: const TextStyle(fontSize: 15.0),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              FilledButton(
+                onPressed: () {
+                  showModalBottomSheet<void>(
+                    context: context,
+                    builder: (context) => SizedBox(
+                      height: 150,
+                      child: Center(
+                        child: Wrap(
+                          crossAxisAlignment: WrapCrossAlignment.start,
+                          spacing: 10,
+                          children: questionQty.asMap().entries.map((entry) {
+                            return InputChip(
+                              label: SizedBox(
+                                width: 80.0,
+                                child: Text(
+                                  textAlign: TextAlign.center,
+                                  entry.value.number.toString(),
+                                  maxLines: 3,
+                                  overflow: TextOverflow.clip,
+                                  softWrap: false,
+                                ),
+                              ),
+                              showCheckmark: true,
+                              onSelected: (bool selected) {
+                                setState(() {});
+                              },
+                            );
+                          }).toList(),
+                        ),
                       ),
-                    );
-                  },
-                  separatorBuilder: (BuildContext context, int index) =>
-                      Divider(
-                    height: 10.0,
-                    color: Theme.of(context).colorScheme.onSurfaceVariant,
-                  ),
-                )
-              : const Center(child: Text("Hello"))),
+                    ),
+                  );
+                },
+                style: headerButtonStyle(context),
+                child: const Text("# Questions"),
+              ),
+              FilledButton(
+                onPressed: () {
+                  showModalBottomSheet<void>(
+                    context: context,
+                    builder: (context) => const SizedBox(
+                      height: 240,
+                      child: Center(
+                        child: CategoryList(),
+                      ),
+                    ),
+                  );
+                },
+                style: headerButtonStyle(context),
+                child: const Text("Categories"),
+              ),
+              FilledButton(
+                onPressed: () {},
+                style: headerButtonStyle(context),
+                child: const Text("Streak"),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 10.0,
+          ),
+          Container(
+              height: MediaQuery.of(context).size.height / 1.50,
+              padding: const EdgeInsets.symmetric(vertical: standardSpacing),
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(minRadius),
+                  topRight: Radius.circular(minRadius),
+                  bottomLeft: Radius.circular(minRadius),
+                  bottomRight: Radius.circular(minRadius),
+                ),
+                color: Theme.of(context).colorScheme.surfaceContainerHigh,
+              ),
+              child: settings
+                  ? ListView.separated(
+                      itemCount: questions.length.toInt(),
+                      itemBuilder: (BuildContext context, int index) {
+                        return ListTile(
+                          title: Text(
+                            questions[index].question,
+                            style: const TextStyle(fontSize: 15.0),
+                          ),
+                        );
+                      },
+                      separatorBuilder: (BuildContext context, int index) =>
+                          Divider(
+                        height: 10.0,
+                        color: Theme.of(context).colorScheme.onSurfaceVariant,
+                      ),
+                    )
+                  : const Center(child: Text("Hello")))
+        ],
+      ),
       bottomNavigationBar: BottomAppBar(
-        height: 85,
+        height: 75,
         color: Colors.transparent,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            const SizedBox(
-              height: 10.0,
-            ),
             FilledButton(
                 style: buttonStyle(context),
                 onPressed: () {
