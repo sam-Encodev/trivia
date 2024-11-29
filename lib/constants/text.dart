d463be1a77c0898eba53f0f0609b90cbcb9f3828
Sam <samuel@encodevlabs.com>
2024-11-29 14:34:05 +0000

    more text

--- lib/constants/text.dart
+++ lib/constants/text.dart
@@ -17,3 +17,20 @@ const system = "System";
 const light = "Light";
 const dark = "Dark";
 const dailyTrivia = "Daily Trivia";
+const feedback = "Feedback";
+const bugReport = "Report a bug";
+const report = "Report";
+
+const markdownData = """
+### How To?
+
+1. Just press the `📧` button.
+2. The feedback view opens.
+You can choose between draw and navigation mode.
+When in navigate mode, you can freely navigate in the
+app. Try it by opening the navigation drawer or by
+tapping the `Open scaffold` button. To switch to the
+drawing mode just press the `Draw` button on the right
+side. Now you can draw on the screen.
+3. To finish your feedback just write a message below and send it by pressing the `Submit` button.
+""";
