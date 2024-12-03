be3dfbd6386703fbee72575b46995e29042906c9
Sam <samuel@encodevlabs.com>
2024-12-03 12:59:35 +0000

    category provider

--- lib/providers/categories.dart
+++ lib/providers/categories.dart
@@ -1,46 +1,19 @@
-import 'package:trivia/models/category.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-class CategoryNotifier extends Notifier<List<Category>> {
+class CategoryNotifier extends Notifier<List<dynamic>> {
   @override
-  List<Category> build() => [
-        Category(
-          name: "Arts & Literature",
-        ),
-        Category(
-          name: "Society & Culture",
-        ),
-        Category(
-          name: "Science",
-        ),
-        Category(
-          name: "Geography",
-        ),
-        Category(
-          name: "Film & TV",
-        ),
-        Category(
-          name: "General Knowledge",
-        ),
-        Category(
-          name: "History",
-        ),
-        Category(
-          name: "Music",
-        ),
-      ].reversed.toList();
+  List build() => [];
 
-  void selected(value) {
-    var active = [];
+  void add(value) {
+    state.add(value);
+  }
 
-    if (value) {
-      active = [...active, value];
-    }
-    print(active);
+  void remove(value) {
+    state.remove(value);
   }
 }
 
 final categoryNotifierProvider =
-    NotifierProvider<CategoryNotifier, List<Category>>(() {
+    NotifierProvider<CategoryNotifier, List<dynamic>>(() {
   return CategoryNotifier();
 });
