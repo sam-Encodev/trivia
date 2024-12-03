aceff0da7e79ff98efd8611cb8f1c49e53a9606a
Sam <samuel@encodevlabs.com>
2024-12-03 13:00:34 +0000

    category filter view

--- lib/components/categories_filter.dart
+++ lib/components/categories_filter.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:trivia/constants/questions.dart';
 import 'package:trivia/providers/categories.dart';
-
-enum ExerciseFilter { walking, running, cycling, hiking }
 
 class CategoryFilter extends ConsumerStatefulWidget {
   const CategoryFilter({super.key});
@@ -12,31 +11,36 @@ class CategoryFilter extends ConsumerStatefulWidget {
 }
 
 class _CategoryFilter extends ConsumerState<CategoryFilter> {
-  Set filters = {};
-
   @override
   Widget build(BuildContext context) {
     final getCategories = ref.watch(categoryNotifierProvider);
 
     return Center(
-      child: Wrap(
-   spacing: 10,
-        children: getCategories.asMap().entries.map((entry) {
-          return FilterChip(
-            showCheckmark: false,
-            label: Text(entry.value.name),
-            selected: filters.contains(entry.value.name),
-            onSelected: (bool selected) {
-              setState(() {
-                if (selected) {
-                  filters.add(entry.value.name);
-                } else {
-                  filters.remove(entry.value.name);
-                }
-              });
-            },
-          );
-        }).toList(),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 5.0),
+        child: Wrap(
+          spacing: 10,
+          children: categoryList.asMap().entries.map((entry) {
+            return FilterChip(
+              showCheckmark: false,
+              label: Text(entry.value),
+              selected: getCategories.contains(entry.value),
+              onSelected: (bool selected) {
+                setState(() {
+                  if (selected) {
+                    ref
+                        .read(categoryNotifierProvider.notifier)
+                        .add(entry.value);
+                  } else {
+                    ref
+                        .read(categoryNotifierProvider.notifier)
+                        .remove(entry.value);
+                  }
+                });
+              },
+            );
+          }).toList(),
+        ),
       ),
     );
   }
